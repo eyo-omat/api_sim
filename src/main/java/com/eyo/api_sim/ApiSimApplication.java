492088bdbea5fa668f38296465156eb99e438169
Eyioyo Omatsola <eyo.omat@gmail.com>
2022-01-19 10:46:22 +0000

    :clown_face: first commit

---
+++ src/main/java/com/eyo/api_sim/ApiSimApplication.java
@@ -0,0 +1,13 @@
+package com.eyo.api_sim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiSimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eyo/api_sim/ApiSimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eyo.api_sim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

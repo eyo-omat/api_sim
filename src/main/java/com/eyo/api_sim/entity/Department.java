51d3eda540481c3c08ba6f9f74a58b20f16b108f
Eyioyo Omatsola <eyo.omat@gmail.com>
2022-01-19 11:28:33 +0000

    :clown_face: Added Lombok dependency Added Department entity

---
+++ src/main/java/com/eyo/api_sim/entity/Department.java
@@ -0,0 +1,41 @@
+package com.eyo.api_sim.entity;
+
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long departmentId;
+    private String departmentName;
+    private String departmentAddress;
+    private String departmentCode;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Department that = (Department) o;
+        return departmentId != null && Objects.equals(departmentId, that.departmentId);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
